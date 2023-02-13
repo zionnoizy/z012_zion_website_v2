92951719a17c5e9a0b8d52f7a621c0e2d9016004
unknown <zionnoizy>
2023-02-13 22:58:07 +0000

    fb

--- src/main.js
+++ src/main.js
@@ -1,19 +1,12 @@
 import { createApp } from 'vue'
-import {createRouter, createWebHistory} from 'vue-router'
+
 import App from './App.vue'
 
-import Home from '@/views/Home.vue'
-import About from '@/views/Empty.vue'
+
 
 import './assets/main.css'
 
-const router = createRouter({
-    history: createWebHistory(),
-    routers: [
-        {path: '/', name: 'Home', component: Home},
-        {path: '/about', name: 'About', component: About}
-    ]
-})
+
 
 createApp(App)
 .use(router)
